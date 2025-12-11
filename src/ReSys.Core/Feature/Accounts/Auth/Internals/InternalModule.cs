0f6b36fdfe70eca71bf7691ff04f11f683440725
Phat Nguyen <phat.nguyen@lockernlock.vn>
2025-12-11 17:12:02 +0700

    feat(auth): implement authentication and session management
    This commit introduces comprehensive authentication and session management
    features for both internal and external (OAuth) providers.
    
    - Add external authentication endpoints for Google and Facebook.
    Implement internal user registration and login.
    Integrate session management for current and all user sessions,
    including token refresh and single/all logout functionality.
    - Provide common account utilities for email confirmation, phone verification,
    and password reset.

---
+++ src/ReSys.Core/Feature/Accounts/Auth/Internals/InternalModule.cs
@@ -0,0 +1,53 @@
+﻿using Carter;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+using ReSys.Core.Common.Models.Wrappers.Responses;
+
+namespace ReSys.Core.Feature.Accounts.Auth.Internals;
+
+public static partial class InternalModule
+{
+    public sealed class Endpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup(prefix: "api/account/auth/internal/")
+                .UseGroupMeta(Annotations.Group)
+                .DisableAntiforgery(); // Disable Anti-forgery if not needed
+
+            group.MapPost(pattern: "register",
+                    handler: RegisterHandler)
+                .UseEndpointMeta(Annotations.Register);
+
+            group.MapPost(pattern: "login",
+                    handler: LoginHandler)
+                .UseEndpointMeta(meta: Annotations.Login);
+
+        }
+
+        private static async Task<Ok<ApiResponse<Login.Result>>> LoginHandler([FromBody] Login.Param param, [FromServices] ISender mediator)
+        {
+            Login.Command command = new Login.Command(Param: param);
+            ErrorOr<Login.Result> result = await mediator.Send(request: command);
+            ApiResponse<Login.Result> apiResponse = result.ToApiResponse(message: "User logged in successfully");
+
+            return TypedResults.Ok(value: apiResponse);
+        }
+
+        private static async Task<Ok<ApiResponse<Register.Result>>> RegisterHandler([FromBody] Register.Command command, [FromServices] ISender mediator)
+        {
+            ErrorOr<Register.Result> result = await mediator.Send(request: command);
+            ApiResponse<Register.Result> apiResponse = result.ToApiResponse(message: "User registered successfully");
+
+            return TypedResults.Ok(value: apiResponse);
+        }
+    }
+}
+
